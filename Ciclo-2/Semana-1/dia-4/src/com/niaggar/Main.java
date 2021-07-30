a31d6806cff6888d725199945dc451ceed4c4d6b
Nicolas Aguilera García <niagar25@gmail.com>
2021-07-29 22:55:11 -0500

    [add] [ciclo-2] Manejo de arreglos y matrices

--- Ciclo-2/Semana-1/dia-4/src/com/niaggar/Main.java
+++ Ciclo-2/Semana-1/dia-4/src/com/niaggar/Main.java
@@ -1,40 +1,47 @@
 package com.niaggar;
 
+import java.util.Scanner;
+
 
 /*
 Temario del dia:
-    -> Cadenas de caracteres y codigo ASCII
-    -> Arreglos y matrices
+    → Cadenas de caracteres y código ASCII
+    → Arreglos y matrices
 */
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola dia 4!");
-        palidromo("ala");
-        palidromo("adios");
-        palidromo("Tenet");
+
+        Scanner sc = new Scanner(System.in);
+
+        arreglos(sc);
+
+        // palidromo("ala");
+        // palidromo("adios");
+        // palidromo("Tenet");
     }
 
+    // UTILIZACIÓN DE CADENAS:
     public static void cadenas_ascii() {
         // Para crear cadenas se hace uso de la clase String
-        // pudiendose crear simplemente indeicandolo o usando su 
-        // constructor. Asi mismo se diferencia de los caracters ya 
+        // pudiéndose crear simplemente indicándolo o usando su
+        // constructor. Asi mismo se diferencia de los caracteres, ya
         // que tienen que declararse con "".
-
         String cadena_1 = "Hola como estas!";
         String cadena_2 = new String("Hasta luego!");
 
-        // Comparacion de cadenas. La forma correcta de comparar
-        // cadenas es mediate el metodo equals(...) auque es posible
+        // Comparación de cadenas. La forma correcta de comparar
+        // cadenas es médiate el método equals(...) aunque es posible
         // utilizar el ==.
         cadena_1.equals(cadena_2);
         cadena_1.equalsIgnoreCase(cadena_2);
 
-        // Verificar la contenecia de una cadena en otra.
+        // Verificar la contenencia de una cadena en otra.
         cadena_1.contains(cadena_2);
 
-        // Extraer un caracter de la cadena, se utiliza el metodo
-        // charAt(i) indicando el indice del caracter (El inidice
+        // Extraer un carácter de la cadena, se utiliza el método
+        // charAt(i) indicando el índice del carácter (El índice
         // inicia en cero).
         cadena_1.charAt(2);
 
@@ -63,5 +70,49 @@ public class Main {
         System.out.println(result);
 
         return result;
+    }
+
+    // UTILIZACIÓN DE ARREGLOS
+    public static void arreglos(Scanner sc) {
+        // Un Arreglo/Vector es una secuencia de n elementos del mismo tipo.
+        // n debe ser definido al crear el arreglo.
+
+        // Representación de un arreglo de tipo int en java:
+        int n = 3;
+        int[] arreglo_enteros = new int[n];
+
+        // Representacion de una matriz de char en java:
+        int x = 10, y = 15;
+        char[][] matriz = new char[x][y];
+
+        // Ejemplo de uso de arreglos
+        int[] res = crear_arreglo_int(sc);
+        imprimir_arreglo_int(res);
+    }
+
+    public static int[] crear_arreglo_int(Scanner sc) {
+        System.out.println("Longitud del arreglo:");
+        int n = sc.nextInt();
+
+        int[] arreglo = new int[n];
+
+        for (int x = 0; x < arreglo.length; x++) {
+            System.out.println("Valor de la posición " + x + ':');
+            arreglo[x] = sc.nextInt();
+        }
+
+        return  arreglo;
+    }
+
+    public static void imprimir_arreglo_int(int[] arreglo) {
+        // StringBuilder: Objeto usado si la cadena de caracteres
+        // va a cambiar.
+        StringBuilder res = new StringBuilder("|");
+
+        for (int x = 0; x < arreglo.length; x++) {
+            res.append(" " + x + ": " + arreglo[x] + " |");
+        }
+
+        System.out.println(res);
     }
 }
